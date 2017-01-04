f1ecf40b1b55e4af86eef77aa4bef20dc239a5f3
Yawar Amin <yawar.amin@gmail.com>
2017-01-04 08:13:52 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const xs = require('xstream').default;
+const run = require('@cycle/xstream-run').run;
+const cycle_dom = require('@cycle/dom');
+const isolate = require('@cycle/isolate').default;
+
+function main() {
+  const sinks = {
+    DOM: xs.periodic(1000).map(i =>
+      cycle_dom.h1('' + i + ' seconds elapsed'))
+  };
+
+  return sinks;
+}
+
+const drivers = { DOM: cycle_dom.makeDOMDriver('#app') };
+
+run(main, drivers);

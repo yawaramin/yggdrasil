9a9761d7408638c8251b1c9c58e572849a07c78a
Yawar Amin <yawar.amin@gmail.com>
2017-01-20 08:29:51 -0500

    Show combined streams issue

--- src/index.js
+++ src/index.js
@@ -3,18 +3,36 @@
 const xs = require('xstream').default;
 const run = require('@cycle/xstream-run').run;
 const cycle_dom = require('@cycle/dom');
-const isolate = require('@cycle/isolate').default;
+const h = cycle_dom.h;
 
-function main() {
-  const sinks = {
-    DOM: xs.periodic(1000).map(i =>
-      cycle_dom.h1('' + i + ' seconds elapsed'))
-  };
+const comment_selector = 'li.comment';
+const comments =
+  xs.of(['Hello, World!', 'Goodbye, cruel world!']).remember();
+
+function num_comments(dom) {
+  return dom
+    .select(comment_selector)
+    .elements()
+    .map(arr => arr.length)
+    .startWith(2);
+}
+
+function view(args) {
+  const num_comments = args[0];
+  const comments = args[1];
+
+  return h(
+    'ul',
+    [h('li', 'Showing ' + num_comments + ' comments.')]
+      .concat(comments.map(comment => h(comment_selector, comment))));
+}
+
+function main(sources) {
+  const sinks =
+    { DOM: xs.combine(num_comments(sources.DOM), comments).map(view) };
 
   return sinks;
 }
 
-const drivers = { DOM: cycle_dom.makeDOMDriver('#app') };
-
-run(main, drivers);
+run(main, { DOM: cycle_dom.makeDOMDriver('#app') });
 
